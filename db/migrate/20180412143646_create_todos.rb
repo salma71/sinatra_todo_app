bd5fe8a09e0ee417017df46afa60075d403176d0
Salma Elshaelshahawy <salma71@gmail.com>
2018-04-12 11:39:34 -0400

    migrate the tables and add NOTES

--- db/migrate/20180412143646_create_todos.rb
+++ db/migrate/20180412143646_create_todos.rb
@@ -1,7 +1,8 @@
 class CreateTodos < ActiveRecord::Migration
   def change
     create_table :todos do |t|
-
+      t.string :chore
+      t.integer :user_id # because chores belongs to a specific user
       t.timestamps null: false
     end
   end
