804cb689d2fc9bc6da5c0295dd2e27de41592485
Salma Elshaelshahawy <salma71@gmail.com>
2018-04-13 11:33:28 -0400

    Edit todo

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,6 +1,6 @@
+# working version
 class TodosController < ApplicationController
-
-  # GET: /todos asking the server for the data in todo -- done 
+  # GET: /todos asking the server for the data in todo -- done
   get "/todos" do
     if signed_in?
       @todos = Todo.all
@@ -8,8 +8,6 @@ class TodosController < ApplicationController
     else
       redirect "/signin"
     end
-
-
   end
 
   # GET: /todos/new -- done
@@ -21,35 +19,54 @@ class TodosController < ApplicationController
     end
   end
 
-  # POST: /todos --- Done
+  # POST: /todos --- done
   post "/todos" do
+    # raise params.inspect
+    #params {"chore"=>"raise params inspect"}
     if signed_in?
       if params[:chore].empty?
         redirect "/todos/new"
       else
-        @user = User.find_by(:id => session[:user_id])
-        @todo = Todo.create(chore: params[:chore], user_id: @user.id)
-        redirect "/todos"
+        # @user = User.find_by(:id => session[:user_id])
+        # create new instance of todo
+        @todo = Todo.new
+        # set the name of chore
+        @todo.chore = params[:chore]
+        # finally save it
+        @todo.save
+        # todo = Todo.create(chore: params[:chore], user_id: @user.id)
+        # redirect to the show page, HTTP is stateless means instance variable in one action
+        # will ever never relates to instance variable in another action
+        redirect "/todos/#{@todo.id}"
       end
     else
       redirect "/signin"
     end
 
   end
-
   # GET: /todos/5
   get "/todos/:id" do
+    @todo = Todo.find(params[:id])
+    # there is no relation between this line and line 37 it just bcz of redirecting due to design
+    # those two values are the end up equals
     erb :"/todos/show.html"
   end
 
+
   # GET: /todos/5/edit
   get "/todos/:id/edit" do
+    @todo = Todo.find(params[:id])
     erb :"/todos/edit.html"
   end
 
   # PATCH: /todos/5
   patch "/todos/:id" do
-    redirect "/todos/:id"
+    # raise params.inspect
+    # fins the todo with the specific id
+    @todo = Todo.find(params[:id])
+    @todo.chore = params[:chore]
+    @todo.save
+      redirect "/todos"
   end
 
   # DELETE: /todos/5/delete
