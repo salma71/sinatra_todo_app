c4d594924fa81279a1a0e5b92b4976b6a04ca43b
Salma Elshaelshahawy <salma71@gmail.com>
2018-04-15 11:11:57 -0400

    add email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   has_many :chores
   has_secure_password
-
-  validates :name, :login, :email, presence: true
+  EMAIL_REGEX = /\A[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}\z/i
+#shortcut validation format
+  validates :name, :presence => true,
+                   :uniqueness => true
+  validates :email, :presence => true,
+                    :format => EMAIL_REGEX
 end
